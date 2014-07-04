cc006e07e636daad3e80ab6e8972b433015e0752
tuanvm54 <only4u1608@gmail.com>
2014-07-04 19:24:17 +0700

    upload

---
+++ Beer.java
@@ -0,0 +1,23 @@
+
+public class Beer {
+	
+	/**
+	 * Method main
+	 *
+	 *
+	 * @param args
+	 *
+	 */
+	public static void main(String[] args) {
+		System.out.println("VuManhTuan"+'\t'+"09020571"+'\t'+"K54CB"+'\t'+"tuanvm54"+'\t'+"only4u1608@gmail.com");
+		int i=0;
+		for(i=9;i>=1;i--)
+		{
+			System.out.println(i+" bottles of beer on the wall, 9 bottles of beer.");
+			System.out.println("Take one down, pass it around, ");
+			if(i==1){
+				System.out.println("No more bottles of beer on the wall.");
+				}
+			}
+	}	
+}
